838824484550b944c3de56830ea5841e543dccec
Saheer Babu <122529965+DevSaheerHost@users.noreply.github.com>
2023-11-27 13:04:27 +0530

    Update main.js

--- chromeHomePage/main.js
+++ chromeHomePage/main.js
@@ -46,7 +46,7 @@ document.body.addEventListener('click',()=>{
 
 function generateRandomWord() {
   // Create an array of words to choose from
-  const words = ["https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230220-223535_Instagram.png", "https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230925-094759_YouTube.jpg", "https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230925-141158_YouTube.jpg", "https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230927-213733_Instagram.jpg", "https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230927-214630_Instagram.jpg"];
+  const words = ["https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230220-223535_Instagram.png", "https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230925-094759_YouTube.jpg", "https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230925-141158_YouTube.jpg", "https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230927-213733_Instagram.jpg", "https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230927-214630_Instagram.jpg","https://devsaheerhost.github.io/WebAssets/chromeHomePage/RandomImages/Screenshot_20230927-214630_Instagram.jpg"];
 
   // Generate a random index from the array
   const randomIndex = Math.floor(Math.random() * words.length);
@@ -65,3 +65,62 @@ document.querySelector('.menuCardImg').src=randomWord
 // const img1= document.createElement('img')
 // menuBig.appendChild(img1)
 // img1.src=randomWord
+
+//morevert
+
+// function moreVert(){
+//   alert('gg')
+// }
+
+const settingsBtn = qs('.settingsBtn')
+const closeStngs = qs('.closeStngs')
+const settings = qs('#settings')
+const main = qs('#main')
+settingsBtn.addEventListener('click', ()=>{
+  settings.classList.toggle('settingsOpen')
+  main.classList.toggle('mainHide')
+})
+
+closeStngs.addEventListener('click',()=>{
+  settings.classList='settingsMain'
+  main.classList='main'
+  
+})
+
+
+const theme = qs('.themeBtn')
+const themeLayout=qs('.themeLayout')
+theme.addEventListener('click',()=>{
+  // alert('wait')
+  toggleThemeLyt()
+})
+
+
+function refresh(){
+  window.location.reload()
+}
+
+const closeTheme = qs('.closeTheme')
+closeTheme.addEventListener('click',()=>{
+  toggleThemeLyt()
+})
+
+function toggleThemeLyt(){
+  themeLayout.classList.toggle('themeLayoutOpen')
+}
+const qsa = e =>document.querySelectorAll(e)
+const body = document.body
+const lightTheme = qs('.lightTheme')
+lightTheme.addEventListener('click',()=>{
+  mainTheme()
+})
+
+const darckTheme = qs('.darckTheme')
+darckTheme.addEventListener('click', () => {
+  
+  mainTheme()
+})
+
+function mainTheme(){
+  document.body.classList.toggle('bodyDarck')
+}
